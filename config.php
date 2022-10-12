82063d6f011fabf2b69ac42cc2b61415305252ac
nphatcampcode <72789405+nphatcampcode@users.noreply.github.com>
2022-10-12 22:24:20 +0700

    Initial commit

---
+++ config.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => 'https://artisanstatic.netlify.app',
+    'site' => [
+        'title' => 'My Jigsaw Blog',
+        'description' => 'Personal blog of John Doe.',
+        'image' => 'default-share.png',
+    ],
+    'owner' => [
+        'name' => 'John Doe',
+    ],
+    'links' => [
+        'twitter' => 'https://twitter.com/johndoe',
+        'github' => 'https://github.com/johndoe',
+    ],
+    'services' => [
+        'cmsVersion' => '~2.10',
+        'analytics' => 'UA-XXXXX-Y',
+        'disqus' => 'artisanstatic',
+        'formcarry' => 'XXXXXXXXXXXX',
+        'cloudinary' => [
+            'cloudName' => 'artisanstatic',
+            'apiKey' => '365895137117119',
+        ],
+    ],
+    'collections' => [
+        'posts' => [
+            'path' => 'posts/{filename}',
+            'sort' => '-date',
+            'extends' => '_layouts.post',
+            'section' => 'postContent',
+            'isPost' => true,
+            'comments' => true,
+            'tags' => [],
+            'hasTag' => function ($page, $tag) {
+                return collect($page->tags)->contains($tag);
+            },
+            'prettyDate' => function ($page, $format = 'M j, Y') {
+                return date($format, $page->date);
+            },
+        ],
+        'tags' => [
+            'path' => 'tags/{filename}',
+            'extends' => '_layouts.tag',
+            'section' => '',
+            'name' => function ($page) {
+                return $page->getFilename();
+            },
+        ],
+    ],
+];
